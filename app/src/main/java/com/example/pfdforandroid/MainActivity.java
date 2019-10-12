c75beb4dc022738bd43b30144b127bfaf4fc8b05
mitchellhall400 <mitchell.hall400@gmail.com>
2019-10-12 12:52:17 -0500

    Factoring pitch into roll

--- app/src/main/java/com/example/pfdforandroid/MainActivity.java
+++ app/src/main/java/com/example/pfdforandroid/MainActivity.java
@@ -20,25 +20,29 @@ import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
     private SensorManager sensorManager;
-    private Sensor gyroscopeSensor;
-    private SensorEventListener gyroScopeEventListener;
+    private Sensor accelerometerSensor;
+    private SensorEventListener accelerometerEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED);
-        if(gyroscopeSensor==null){
-            Toast.makeText(this, "This device ain't got no gyroscope",Toast.LENGTH_SHORT).show();
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED);
+        if(accelerometerSensor==null){
+            Toast.makeText(this, "This device doesn't have an accelerometer.",Toast.LENGTH_SHORT).show();
             finish();
         }
-        gyroScopeEventListener = new SensorEventListener() {
+        accelerometerEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
                 View artHorizGround = (View)findViewById(R.id.artHorizGround);
-                artHorizGround.getLayoutParams().height = ((int)(event.values[2]*100)+1500);
+
+                /* Set Pitch */
+                artHorizGround.getLayoutParams().height = (int)(((event.values[2]*100)+1300) / event.values[0]);
                 artHorizGround.requestLayout();
+
+                /* Set Roll */
                 artHorizGround.setRotation(event.values[0]*8);
             }
 
@@ -52,11 +56,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        sensorManager.registerListener(gyroScopeEventListener,gyroscopeSensor,SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(accelerometerEventListener,accelerometerSensor,SensorManager.SENSOR_DELAY_FASTEST);
     }
     @Override
     protected void onPause(){
         super.onPause();
-        sensorManager.unregisterListener(gyroScopeEventListener);
+        sensorManager.unregisterListener(accelerometerEventListener);
     }
 }
