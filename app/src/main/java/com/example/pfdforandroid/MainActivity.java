c5fea362ab41b74446c24b5accbcd2f7986e2bab
mitchellhall400 <mitchell.hall400@gmail.com>
2019-10-12 12:34:18 -0500

    Fixed roll

--- app/src/main/java/com/example/pfdforandroid/MainActivity.java
+++ app/src/main/java/com/example/pfdforandroid/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED);
         if(gyroscopeSensor==null){
             Toast.makeText(this, "This device ain't got no gyroscope",Toast.LENGTH_SHORT).show();
             finish();
@@ -36,20 +36,10 @@ public class MainActivity extends AppCompatActivity {
         gyroScopeEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-//                final LayoutParams layoutparams = (RelativeLayout.LayoutParams) Txt1.getLayoutParams();
-//
-//                if(event.values[2] > 0.5f){
-//                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-//                }
-//                else if(event.values[2] < -0.5f){
-//                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-//
                 View artHorizGround = (View)findViewById(R.id.artHorizGround);
-                artHorizGround.getLayoutParams().height = ((int)(event.values[2]*100)+800);
+                artHorizGround.getLayoutParams().height = ((int)(event.values[2]*100)+1500);
                 artHorizGround.requestLayout();
-                artHorizGround.setRotation(-((event.values[1]-9)*10));
-                TextView text =(TextView)findViewById(R.id.textView);
-                text.setText(Integer.toString((int) (event.values[1])));
+                artHorizGround.setRotation(event.values[0]*8);
             }
 
             @Override
