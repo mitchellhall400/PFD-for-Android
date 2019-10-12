89876cfabee64a9d5997e4ce67251dc20784efe6
mitchellhall400 <mitchell.hall400@gmail.com>
2019-10-12 12:01:07 -0500

    Added gyros responding to height

--- app/src/main/java/com/example/pfdforandroid/MainActivity.java
+++ app/src/main/java/com/example/pfdforandroid/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.pfdforandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.graphics.Color;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.Sensor;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
+import java.lang.String;
+import android.view.ViewGroup.LayoutParams;
+
+import static java.lang.Math.abs;
+
 
 public class MainActivity extends AppCompatActivity {
     private SensorManager sensorManager;
@@ -28,12 +36,20 @@ public class MainActivity extends AppCompatActivity {
         gyroScopeEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if(event.values[2] > 0.5f){
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                }
-                else if(event.values[2] < -0.5f){
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-                }
+//                final LayoutParams layoutparams = (RelativeLayout.LayoutParams) Txt1.getLayoutParams();
+//
+//                if(event.values[2] > 0.5f){
+//                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+//                }
+//                else if(event.values[2] < -0.5f){
+//                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+//
+                View artHorizGround = (View)findViewById(R.id.artHorizGround);
+                artHorizGround.getLayoutParams().height = ((int)(event.values[2]*100)+800);
+                artHorizGround.requestLayout();
+                artHorizGround.setRotation(-((event.values[1]-9)*10));
+                TextView text =(TextView)findViewById(R.id.textView);
+                text.setText(Integer.toString((int) (event.values[1])));
             }
 
             @Override
